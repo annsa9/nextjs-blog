28f6dfff0ff7098b06539378d5be6260b3cb924d
arun <arun.edu01@gmail.com>
2020-06-08 11:21:55 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// To remove unused CSS by specifying the purge option
+module.exports = {
+  purge: [
+    // Use *.tsx if using TypeScript
+    "./pages/**/*.js",
+    "./components/**/*.js",
+  ],
+  // ...
+};
