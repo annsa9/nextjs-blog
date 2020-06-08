80e604246d754116b500e5a3988dd7e53a28d8b7
arun <arun.edu01@gmail.com>
2020-06-08 12:10:16 +0530

    Add pwa in next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   distDir: "build",
-};
+  pwa: {
+    dest: "public",
+  }
+});
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -6,6 +6,43 @@ export default function FirstPost() {
     <Layout>
       <Head>
         <title>First Post</title>
+        <meta name="application-name" content="Next PWA App" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Next PWA App" />
+        <meta name="description" content="Best Next PWA App in the world" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/static/manifest.json" />
+        <link
+          rel="mask-icon"
+          href="/static/icons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/static/icons/favicon.ico" />
       </Head>
       <h1>First Post</h1>
     </Layout>
